a008842b1df26c7626e4a265fd3675012089c6d1
EC2 Default User <ec2-user@ip-172-31-34-219.us-east-2.compute.internal>
2018-02-23 08:24:03 +0000

    hw2 part 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # @movies = Movie.all
+    # Use params[] hash to sort
+    @movies = Movie.find(:all, :order => (params[:sort_by]))
+    @sort_column = params[:sort_by]
   end
 
   def new
