9e5582c0962a16764d019fa7d5e1aab8c86cc5e1
EC2 Default User <ec2-user@ip-172-31-34-219.us-east-2.compute.internal>
2018-02-24 21:24:23 +0000

    hw2 part2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,8 +14,16 @@ class MoviesController < ApplicationController
     #@movies = Movie.all
     # Use params[] hash to sort
     @movies = Movie.order(params[:sort_by])
-    #@movies = Movie.find(:all, :order => (params[:sort_by]))
+    if params[:ratings]
+	# @movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+	@movies = Movie.where(:rating => params[:ratings].keys).order(params[:sort_by])
+    end
     @sort_column = params[:sort_by]
+    @all_ratings = Movie.all_ratings
+    @set_ratings = params[:ratings]
+    if !@set_ratings
+	@set_ratings = Hash.new
+    end
   end
 
   def new
